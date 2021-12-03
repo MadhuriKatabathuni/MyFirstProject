445c7347879910c1b295c2c075076cfef2915b3b
Madhuri <madhu.k19@gmail.com>
2021-12-03 10:39:08 +0530

    added 1st program to check the changes on github

---
+++ pOper1.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+c = a + b # performing addition operator
+print(c, "Addition")
+d = a - b # performing subtracction operator
+print(d, "Subraction")
+e = a * b # performing multiplication operator
+print(e, "Multiplication")
+f = a / b # performing division operator
+print(f, "Division")
