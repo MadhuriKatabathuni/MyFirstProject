382e7e7b3e95a9e42836b60fbe14854e382474a1
Madhuri <madhu.k19@gmail.com>
2021-12-03 09:35:10 +0530

    First Commit

---
+++ pbasics2.py
@@ -0,0 +1,6 @@
+print("This is my second program in python");
+# This is single line comment
+print("Checking multi line comments");
+# This is multi line comment
+# This program is to show single and multi line comments
+# For every multi line use hash symbol
---
+++ pbasics3.py
@@ -0,0 +1,16 @@
+age = 28
+commission = 40.23
+is_male = True
+message = "Have a nice day"
+my_list = ["apples", 5, 7.3]
+my_dictionary = {'amount': 45}
+coordinates = (30, 60)
+my_set = {5, 6, 3}
+print("My age is: " + str(age));
+print("I got my comm : " + str(commission));
+print(str(message));
+print("Displaying my list :");
+print('\n'.join(map(str,my_list)));
+print("Displaying dictionary value :");
+print(my_dictionary["amount"]); 
+print(my_set);
